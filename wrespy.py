f0c7122bf3bce9a7bdf2bacaefa5303bf5ca57e6
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2026-01-04 15:25:38 -0500

    Remove dependencies on nefile and pefile - we will just use wrestool for now

--- wrespy.py
+++ wrespy.py
@@ -68,17 +68,12 @@ def extract_with_wrestool(filepath, output_dir):
         print("Error: wrestool not found. Please install icoutils.", file=sys.stderr)
         sys.exit(1)
 
-def extract_ne_resources(filepath, output_dir):
-    """Extract resources from an NE file using nefile."""
-    executable = nefile.NE(filepath)
-    print(f"NE: {filepath}")
-    executable.export_resources(output_dir)
+def extract_resources(filepath, output_dir):
+    extract_with_wrestool(filepath, output_dir)
 
-# Updated process_file function to handle CAB files
 def process_file(filepath, output_dir):
-    """Process a single file and extract resources if it's a valid PE, NE, or CAB file."""
+    """Process a single file and extract resources if it's a valid PE or CAB file."""
     # Check if the file is a CAB file
-    # TODO: Handle other common installation archive types.
     if filepath.lower().endswith('.cab'):
         with tempfile.TemporaryDirectory() as temp_dir:
             try:
@@ -100,36 +95,6 @@ def process_file(filepath, output_dir):
 
     # Otherwise, attempt to extract any resources that might be in here.
     extract_resources(filepath, output_dir)
-
-# Currently we are trying to extract just with wrestool and not using nefile for now.
-# That might actually be sufficient for our needs.
-def extract_resources(filepath, output_dir):
-    extract_with_wrestool(filepath, output_dir)
-
-# This is currently not used, as the issues with wrestool maybe were resolved? Not sure yet.
-def extract_resources_with_wrestool_or_nefile(filepath, output_dir):
-    # For development purposes, we can install a local version of the library with this:
-    #  pipenv install -e ../nefile
-    import nefile
-
-    # DETECT THE FILE TYPE.
-    # We will first assume this is a PE file, and if loading as a PE fails we will try reading as an NE file.
-    try:
-        # Try to load as PE file. We are not currently using the pefile library to extract the resources; just
-        # to check the format. But in the longer term, we should have our own extraction and not use wrestool
-        # because of the well-known issues with it.
-        executable = pefile.PE(filepath)
-        print(f"PE: {filepath}")
-        extract_with_wrestool(filepath, output_dir)
-
-    except pefile.PEFormatError:
-        # Not a PE file, try NE.
-        try:
-            extract_ne_resources(filepath, output_dir)
-
-        except:
-            # Not a valid PE or NE file, so just skip it.
-            print(f"Skipped: {filepath}")
 
 def process_path(input_path, output_dir=None):
     """Process a file or directory recursively."""
